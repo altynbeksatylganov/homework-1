7dea21356564958272195290befd993aaa1d326b
Altynbek Satylganov <altynbeksatylganov92@gmail.com>
2026-02-13 20:58:22 +0500

    final

--- src/main/java/com/narxoz/rpg/equipment/IronSword.java
+++ src/main/java/com/narxoz/rpg/equipment/IronSword.java
@@ -1,28 +1,5 @@
 package com.narxoz.rpg.equipment;
 
-/**
- * Example concrete weapon implementation - Medieval theme.
- *
- * This is provided as a reference to help you understand the structure.
- * Study this, then create other weapons and armor for different themes.
- *
- * Notice:
- * - This implements the Weapon interface
- * - It has specific attributes for this weapon type
- * - It belongs to the "Medieval" equipment family
- *
- * TODO: Create similar implementations for:
- * Weapons:
- * - WizardStaff (Magic theme)
- * - Longbow (Ranger theme)
- * - Other weapons you imagine...
- *
- * Armor:
- * - PlateArmor (Medieval theme)
- * - EnchantedRobes (Magic theme)
- * - LeatherArmor (Ranger theme)
- * - Other armor you imagine...
- */
 public class IronSword implements Weapon {
 
     private final int damage;
